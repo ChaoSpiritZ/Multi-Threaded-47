bce0ffe78d593457f73eefe55cc01ccf9ddf6c7f
Yoav <47596599+ChaoSpiritZ@users.noreply.github.com>
2020-02-14 21:44:08 +0200

    Add files via upload

---
+++ Multi Threaded 47/Multi Threaded 47-2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multi_Threaded_47_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task t1 = WritetoFile();
+            
+            while (!t1.Wait(100))
+            {
+                Console.WriteLine(".");
+            }
+        }
+
+        static async Task WritetoFile()
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            using (StreamWriter outputFile = new StreamWriter(Path.Combine(docPath, "hellothere.txt")))
+            {
+                Console.WriteLine("Building...");
+                for (int i = 0; i <= 1000; i++)
+                {
+                    await outputFile.WriteLineAsync(i.ToString());
+                    Thread.Sleep(2);
+                }
+                
+                Console.WriteLine("Construction complete!");
+            }
+        }
+    }
+}
---
+++ Multi Threaded 47/Multi Threaded 47/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multi_Threaded_47
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await Task.Run(() =>
+            {
+                button1.Visible = false;
+                int milisecondsPassed = 0;
+                while (true)
+                {
+                    milisecondsPassed += 100;
+                    label1.Text = DateTime.Now.ToString();
+                    
+                    if(milisecondsPassed % 200 == 0)
+                        label1.ForeColor = Color.Green;
+                    else
+                        label1.ForeColor = Color.Red;
+
+                    Thread.Sleep(100);
+                }
+            });
+        }
+    }
+}
